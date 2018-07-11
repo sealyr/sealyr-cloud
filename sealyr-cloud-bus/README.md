a1647616b7cca2184cd2e4cb005bba4f4bcd9af4
zhycn <qzhservice@gmail.com>
2018-07-11 21:17:11 +0800

    Add bus.

--- sealyr-cloud-bus/README.md
+++ sealyr-cloud-bus/README.md
@@ -1,1 +1,3 @@
 # sealyr-cloud-bus
+
+http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__spring_cloud_bus.html
--- sealyr-cloud-bus/src/main/java/com/sealyr/framework/cloud/bus/BusApplication.java
+++ sealyr-cloud-bus/src/main/java/com/sealyr/framework/cloud/bus/BusApplication.java
@@ -2,10 +2,8 @@ package com.sealyr.framework.cloud.bus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringCloudApplication
-@EnableHystrixDashboard
 public class BusApplication {
 
   public static void main(String[] args) {
