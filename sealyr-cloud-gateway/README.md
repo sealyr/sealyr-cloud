4eabbcdfb057d6539cee63a6bb3a9d86f600be28
zhycn <qzhservice@gmail.com>
2018-07-11 22:22:08 +0800

    Initial gateway commit.

--- sealyr-cloud-gateway/README.md
+++ sealyr-cloud-gateway/README.md
@@ -1,1 +1,3 @@
 # sealyr-cloud-gateway
+
+http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__spring_cloud_gateway.html
--- sealyr-cloud-gateway/sealyr-cloud-gateway-server/src/main/java/com/sealyr/framework/cloud/gateway/server/GatewayServerApplication.java
+++ sealyr-cloud-gateway/sealyr-cloud-gateway-server/src/main/java/com/sealyr/framework/cloud/gateway/server/GatewayServerApplication.java
@@ -1,12 +1,13 @@
 package com.sealyr.framework.cloud.gateway.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@SpringCloudApplication
 public class GatewayServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(GatewayServerApplication.class, args);
   }
+
 }
