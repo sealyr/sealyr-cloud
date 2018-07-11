f7a542a2ce3f0a0aecece56e9816c7088cdf075b
zhycn <qzhservice@gmail.com>
2018-07-11 20:47:08 +0800

    Add config.

--- sealyr-cloud-config/README.md
+++ sealyr-cloud-config/README.md
@@ -1,1 +1,3 @@
 # sealyr-cloud-config
+
+http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__spring_cloud_config.html
--- sealyr-cloud-config/sealyr-cloud-config-server/src/main/java/com/sealyr/framework/cloud/config/server/ConfigServerApplication.java
+++ sealyr-cloud-config/sealyr-cloud-config-server/src/main/java/com/sealyr/framework/cloud/config/server/ConfigServerApplication.java
@@ -3,10 +3,8 @@ package com.sealyr.framework.cloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringCloudApplication
-@EnableHystrixDashboard
 @EnableConfigServer
 public class ConfigServerApplication {
 
