efda9e925f95e3ac4228082b1e0e3e15ba9de359
zhycn <qzhservice@gmail.com>
2018-07-11 20:24:48 +0800

    Add zuul.

--- sealyr-cloud-netflix/sealyr-cloud-netflix-zuul/README.md
+++ sealyr-cloud-netflix/sealyr-cloud-netflix-zuul/README.md
@@ -1,1 +1,3 @@
 # sealyr-cloud-netflix-zuul
+
+http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__router_and_filter_zuul.html
--- sealyr-cloud-netflix/sealyr-cloud-netflix-zuul/src/main/java/com/sealyr/framework/cloud/netflix/zuul/ZuulApplication.java
+++ sealyr-cloud-netflix/sealyr-cloud-netflix-zuul/src/main/java/com/sealyr/framework/cloud/netflix/zuul/ZuulApplication.java
@@ -2,11 +2,9 @@ package com.sealyr.framework.cloud.netflix.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
-@EnableHystrixDashboard
 @EnableZuulProxy
 public class ZuulApplication {
 
